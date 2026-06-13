6f25ad1c829288016f18da010d73652eb1a05f22
Vitalii Kotenko <caunt.official@gmail.com>
2026-06-14 01:23:19 +0300

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -2,7 +2,7 @@ using CurseForgeProxy;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Logging.SetMinimumLevel(LogLevel.Trace);
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
 builder.Services.AddSingleton<EnvironmentConfiguration>();
 builder.Services.AddSingleton<CurseForgeEndpoints>();
